8d1f6e1a805a8a372b442dd4550cac6e4497b2e2
Marco <macrozone@gmail.com>
2015-12-02 14:14:55 +0100

    add version constraint

--- package.js
+++ package.js
@@ -12,7 +12,7 @@ Package.describe({
 
 Package.onUse(function(api) {
   api.versionsFrom('1.2.1');
-  api.use(['ecmascript', 'less', 'orionjs:core', 'reactive-var']);
+  api.use(['ecmascript', 'less', 'orionjs:core@1.7.0', 'reactive-var']);
   api.addFiles('orion-components.html');
   api.addFiles('orion-components.js');
   api.addFiles('orion-components.less');
