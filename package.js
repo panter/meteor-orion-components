0dcab602fc9640de806f193f6238e97b87ce31df
Marco <macrozone@gmail.com>
2016-02-07 18:14:02 +0100

    update version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'panter:orion-components',
-  version: '0.0.2',
+  version: '0.1.0',
   // Brief, one-line summary of the package.
   summary: 'create components in orion',
   // URL to the Git repository containing the source code for this package.
